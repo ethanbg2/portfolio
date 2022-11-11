d0a20a3f5d402a586f2eb1ad16a753f4ab842b46
ethanbg2 <ethanbg2@illinois.edu>
2022-11-10 22:19:50 -0600

    created mobile friendly menu

--- src/components/burger-menu.vue
+++ src/components/burger-menu.vue
@@ -1,17 +1,27 @@
 <template>
   <div data-role="BurgerMenu" class="burger-menu-burger-menu">
-    <svg viewBox="0 0 1024 1024" class="burger-menu-icon">
+    <navigation-links ></navigation-links>
+    <!-- <svg viewBox="0 0 1024 1024" class="burger-menu-icon">
       <path
         d="M128 554.667h768c23.552 0 42.667-19.115 42.667-42.667s-19.115-42.667-42.667-42.667h-768c-23.552 0-42.667 19.115-42.667 42.667s19.115 42.667 42.667 42.667zM128 298.667h768c23.552 0 42.667-19.115 42.667-42.667s-19.115-42.667-42.667-42.667h-768c-23.552 0-42.667 19.115-42.667 42.667s19.115 42.667 42.667 42.667zM128 810.667h768c23.552 0 42.667-19.115 42.667-42.667s-19.115-42.667-42.667-42.667h-768c-23.552 0-42.667 19.115-42.667 42.667s19.115 42.667 42.667 42.667z"
       ></path>
-    </svg>
+    </svg> -->
   </div>
 </template>
 
 <script>
+import NavigationLinks from './navigation-links'
 export default {
   name: 'BurgerMenu',
   props: {},
+  components: {
+    NavigationLinks
+  },
+  data() {
+    return {
+      show_menu: true
+    }
+  }
 }
 </script>
 
